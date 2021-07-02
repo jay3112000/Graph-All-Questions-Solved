1aaa9cc8c107fe2769c56cc495f9cf63d4b26d78
Jay Sharma <61292543+jay3112000@users.noreply.github.com>
2021-07-02 05:01:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -42,6 +42,143 @@ class Graph:
                 if i not in visited and self.adj[s][i] == 1:
                     q.append(i)
                     visited.add(i)
+    def topological_sort_util(self,v,visited,stack):
+        visited.add(v)
+        for i in range(self.size):
+            if i not in visited:
+                self.topological_sort_util(i,visited,stack)
+        stack.append(v)
+
+    def topological_sort(self):
+        stack=[]
+        visited=set()
+        for i in range(self.size):
+            if i not in visited:
+                self.topological_sort_util(i,visited,stack)
+        print(stack[::-1])
+
+    def isPath(self,src,dest):
+        visited=set()
+        q=[]
+        visited.add(src)
+        q.append(src)
+        while len(q)>0:
+            temp = q.pop(0)
+            for i in range(self.size):
+                if temp == dest:
+                    return True
+                if i not in visited and self.adj[temp][i] == 1:
+                    q.append(i)
+                    visited.add(i)
+        return False
+    def allPathsUtil(self,src,dest,visited,path):
+        visited.add(src)
+        path.append(src)
+        if src==dest:
+            print(path)
+
+        for i in range(self.size):
+            if self.adj[src][i] == 1 and i not in visited:
+                self.allPathsUtil(i,dest,visited,path)
+        path.pop()
+        visited.remove(src)
+
+    def allPaths(self,src,dest):
+        visited = set()
+        path=[]
+        self.allPathsUtil(src,dest,visited,path)
+    def shortes_path_util(self,src,dest,visited,path,ans):
+        visited.add(src)
+        path.append(src)
+        if src==dest:
+            ans.append((len(path)))
+        for i in range(self.size):
+            if self.adj[src][i]==1 and i not in visited:
+                self.shortes_path_util(i,dest,visited,path,ans)
+        path.pop()
+        visited.remove(src)
+        return(min(ans))
+
+
+    def shortest_path(self,src,dest):
+        visited=set()
+        path=[]
+        ans=[]
+        print(self.shortes_path_util(src,dest,visited,path,ans))
+
+    def detect_util(self,s,visited,parent):
+        visited.add(s)
+        parent.append(s)
+        for i in range(self.size):
+            if self.adj[s][i]==1:
+                if i not in visited:
+                    parent[i]=s
+                    self.detect_util(i,visited,parent)
+                elif parent[s]!=i:
+                    return True
+        return False
+
+    def detect_cycle(self,s):
+        visited=set()
+        flag=0
+        parent = [-1] * self.size
+        for i in range(self.size):
+            if i not in visited and self.detect_util(i,visited,parent):
+                flag=1
+                break
+        if flag==1:
+            return('Yes')
+        else:
+            return('No')
+
+
+    def isValid(self,visited,i,j,N,M,mat):
+        if i>=0 and j>=0 and i<N and j<M and visited[i][j]==False and mat[i][j]==1:
+            return  True
+        return False
+
+
+    def Number_Islands_Util(self,visited,i,j,N,M,mat):
+        row=[1,1,0,-1,-1,0,-1,1]
+        col=[-1,0,-1,0,-1,1,1,1]
+        q=[]
+        q.append((i,j))
+        visited[i][j]=True
+        while len(q)>0:
+            x,y=q.pop(0)
+            for k in range(8):
+                if self.isValid(visited,x+row[k],y+col[k],N,M,mat):
+                    visited[x+row[k]][y+col[k]]=True
+                    q.append((x+row[k],y+col[k]))
+
+
+
+
+
+
+
+    def Number_Islands(self):
+        mat=[
+            [1,1,1,0],
+            [0,1,0,0],
+            [1,1,0,0],
+            [0,0,1,1],
+            [0,0,0,0],
+            [1,0,0,0]
+        ]
+        N=6
+        M=4
+        islands=0
+        visited=[[False for x in range(M)] for y in range(N)]
+        for i in range(N):
+            for j in range(M):
+                if visited[i][j]==False and mat[i][j]==1:
+                    self.Number_Islands_Util(visited,i,j,N,M,mat)
+                    islands+=1
+        print(islands)
+
+
+
 
 
 def main():
@@ -52,8 +189,8 @@ def main():
         for i in range(e):
             u, v = map(int, input().split())
             g.add_edge(u, v)
+        g.Number_Islands()
 
-        g.dfs(0)
 
 
 if __name__ == '__main__':
